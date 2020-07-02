3c9104014f424efbba84f5a3418f677a70dd5d22
NightDays <1257164071@qq.com>
2020-07-02 15:06:15 +0800

    升级LARAVEL-ADMIN

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -8,6 +8,7 @@ use App\Models\UserAddress;
 use App\Policies\UserAddressPolicy;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -29,6 +30,7 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+        Schema::defaultStringLength(191);
 
         //
     }
